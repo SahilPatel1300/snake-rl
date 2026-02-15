d7ae6738d379fae1793382bea535d8c259d619da
Sahil <sahilp.patel1300@gmail.com>
2026-02-15 18:22:28 -0500

    basic functions, reset function, food function

---
+++ game.py
@@ -0,0 +1,58 @@
+import random
+import pygame
+from enum import Enum
+from collections import namedtuple
+
+pygame.init()
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
+
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+BLOCK_SIZE = 20
+SPEED = 40
+
+class SnakeGameAI: 
+
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+
+        # init display 
+        self.display = pygame.display.setmode(self.w, self.h)
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+        self.reset()
+
+
+    def reset(self): 
+        # init game state
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, Point(self.head.x-BLOCK_SIZE, self.head.y), Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0 
+
+    def _place_food(self):
+        while True:
+            x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+            y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+            self.food = Point(x, y)
+
+            if self.food not in self.snake:
+                break
