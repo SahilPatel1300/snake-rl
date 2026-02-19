d9cd08243f21b3dffd1b12253200a1e35f524256
Sahil <sahilp.patel1300@gmail.com>
2026-02-18 20:06:56 -0500

    implemented remember, train_long_memory, train_short_memory, get_action

--- agent.py
+++ agent.py
@@ -15,11 +15,13 @@ class Agent:
 
     def __init__(self):
         self.number_of_games = 0
-        self.espilon = 0 # randomness
+        self.epsilon = 0 # randomness
         self.gamma = 0 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         print(f"Agent initialized with max memory {MAX_MEMORY}\n Batch size {BATCH_SIZE}\n Learning rate {LR}")
-        #TODO: model and trainer 
+        self.model = None #TODO 
+        self.trainer = None #TODO
+        
 
 
     def get_state(self, game): 
@@ -37,22 +39,22 @@ class Agent:
 
         state = [
             # danger straight 
-            (dir_r and game.is_collison(point_r)) or
-            (dir_l and game.is_collison(point_l)) or
-            (dir_u and game.is_collison(point_u)) or
-            (dir_d and game.is_collison(point_d)), 
+            (dir_r and game.is_collision(point_r)) or
+            (dir_l and game.is_collision(point_l)) or
+            (dir_u and game.is_collision(point_u)) or
+            (dir_d and game.is_collision(point_d)), 
 
             # danger right 
-            (dir_u and game.is_collison(point_r)) or
-            (dir_d and game.is_collison(point_l)) or
-            (dir_l and game.is_collison(point_u)) or
-            (dir_r and game.is_collison(point_d)), 
+            (dir_u and game.is_collision(point_r)) or
+            (dir_d and game.is_collision(point_l)) or
+            (dir_l and game.is_collision(point_u)) or
+            (dir_r and game.is_collision(point_d)), 
 
             # danger left 
-            (dir_d and game.is_collison(point_r)) or
-            (dir_u and game.is_collison(point_l)) or
-            (dir_r and game.is_collison(point_u)) or
-            (dir_l and game.is_collison(point_d)), 
+            (dir_d and game.is_collision(point_r)) or
+            (dir_u and game.is_collision(point_l)) or
+            (dir_r and game.is_collision(point_u)) or
+            (dir_l and game.is_collision(point_d)), 
 
             # move direction
             dir_l, 
@@ -70,17 +72,53 @@ class Agent:
         return np.array(state, dtype=int)
     
 
-    def remember(self, state, action, reward, next_state, done):
-        pass
+    def remember(self, state, action, reward, next_state, done) -> None:
+        self.memory.append((state, action, reward, next_state, done)) # popleft if max mem 
     
-    def train_long_memory(self):
-        pass
+    def _unzip (self, sample): 
+        if not sample: 
+            return ()
+        
+        num_fields = len(sample[0])
+        result = [[] for _ in range(num_fields)]
 
-    def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        for item in sample: 
+            for i in range(num_fields): 
+                result[i].append(item[i])
+        
+        return tuple(tuple(field) for field in result)
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuiples 
+        else: 
+            mini_sample = self.memory
+        
+        # extract vars 
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+        
+
+    def train_short_memory(self, state, action, reward, next_state, done) -> None:
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        # random moves: tradeoff expoloration and exploitation 
+        # TODO play around with this 
+
+        # smaller the epsilon, less frequent of random moves 
+        self.epsilon = 80 - self.number_of_games
+        final_move = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else: 
+            state0 = torch.tensor(state, dtype = torch.float)
+            predication = self.model.predict(state0)
+            move = torch.argmax(predication).item()
+            final_move[move] = 1
+
+        return final_move
 
 def train():
     # initialize plot scores and agent
