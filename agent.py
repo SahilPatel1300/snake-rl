e21e1e09f0f92b4a592935bf2b2a6977c35eb9d8
Sahil <sahilp.patel1300@gmail.com>
2026-02-18 19:38:47 -0500

    state array

--- agent.py
+++ agent.py
@@ -23,6 +23,50 @@ class Agent:
 
 
     def get_state(self, game): 
+        head = game.snake[0]
+        # check if boundary/food might get hit
+        point_l = Point(head.x - 20, head.y)
+        point_r = Point(head.x + 20,  head.y)
+        point_u = Point(head.x, head.y - 20)
+        point_d = Point(head.x, head.y+20)
+        # current direction 
+        dir_l = game.direction == Direction.LEFT
+        dir_r = game.direction == Direction.RIGHT
+        dir_u = game.direction == Direction.UP
+        dir_d = game.direction == Direction.DOWN
+
+        state = [
+            # danger straight 
+            (dir_r and game.is_collison(point_r)) or
+            (dir_l and game.is_collison(point_l)) or
+            (dir_u and game.is_collison(point_u)) or
+            (dir_d and game.is_collison(point_d)), 
+
+            # danger right 
+            (dir_u and game.is_collison(point_r)) or
+            (dir_d and game.is_collison(point_l)) or
+            (dir_l and game.is_collison(point_u)) or
+            (dir_r and game.is_collison(point_d)), 
+
+            # danger left 
+            (dir_d and game.is_collison(point_r)) or
+            (dir_u and game.is_collison(point_l)) or
+            (dir_r and game.is_collison(point_u)) or
+            (dir_l and game.is_collison(point_d)), 
+
+            # move direction
+            dir_l, 
+            dir_r,
+            dir_u,
+            dir_d,
+            
+            # food location 
+            game.food.x < game.head.x, # food left 
+            game.food.x > game.head.x, # right
+            game.food.y < game.head.y, # food up
+            game.food.y > game.food.y # food down 
+        ]
+
         pass
 
     def remember(self, state, action, reward, next_state, done):
